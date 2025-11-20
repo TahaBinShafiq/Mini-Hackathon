1f515094c353baa3876faed930a9d48abf07431e
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-11-20 14:49:06 +0500

    Add loading

--- src/app/pages-partials/dashboard/index.js
+++ src/app/pages-partials/dashboard/index.js
@@ -1,27 +1,31 @@
 'use client'
 import UserProfileDropdown from "@/components/ui/user-profile-dropdown";
 import { getAuth, signOut } from "firebase/auth";
+import { useState } from "react";
 import toast from "react-hot-toast";
+import ThreeDotLoader from "../loader";
 
 
 export default function DashboardPage() {
 
+    const[loading , setLoading] = useState(false)
     const logOutfunc = () => {
-
+        setLoading(true)
         const auth = getAuth();
         signOut(auth).then(() => {
             // Sign-out successful.
             toast.success('Log Out Successfully!');
+            setLoading(false)
         }).catch((error) => {
             // An error happened.
+            setLoading(false)
         });
     }
     return (
-        <>
-            <div className="flex justify-between p-2">
+        <>{loading === false ?  <div className="flex justify-between p-2">
                 <h1 className="text-4xl">Dashboard Page</h1>
                 <UserProfileDropdown logOut={logOutfunc}/>
-            </div>
+            </div> : <><div className="flex justify-center items-center h-screen"><ThreeDotLoader/></div></>}
         </>
     )
 }
